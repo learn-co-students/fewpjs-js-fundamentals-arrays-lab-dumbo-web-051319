53f12b4fda2a6917449663ea563dad17314f474a
github username <github email address>
2019-06-23 14:48:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your solution here!
+const arr = ["Milo", "Otis", "Garfield"],
+  append = [...arr, "Odie"],
+  prepend = ["Odie", ...arr],
+  removeFirst = [...arr.slice(1)],
+  removeLast = [...arr.slice(0, -1)];
